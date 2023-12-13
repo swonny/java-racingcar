0cd03c8cd1b0fccdca2b4949571c1f28e38c4099
swonny <wonny921@gmail.com>
2023-12-13 15:25:39 +0900

    refactor: 패키지명 변경

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,10 @@ public class Cars {
         this.cars = cars;
     }
 
+    public void moveAll(final MoveStrategy moveStrategy) {
+        cars.forEach(car -> car.move(moveStrategy.generate()));
+    }
+
     public List<Car> calculateWinners() {
         final int maxPosition = cars.stream()
                                     .map(Car::getPosition)
@@ -21,5 +26,9 @@ public class Cars {
         return cars.stream()
                    .filter(car -> car.getPosition() == maxPosition)
                    .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
     }
 }
